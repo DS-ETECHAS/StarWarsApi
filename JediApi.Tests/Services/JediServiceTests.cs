bb71ccc4c45bc0dcaba57cb93fe19a623cb5d43a
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-06-17 11:50:36 -0300

    test(services): add empty methods

---
+++ JediApi.Tests/Services/JediServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using JediApi.Models;
+using JediApi.Repositories;
+using JediApi.Services;
+using Moq;
+
+namespace JediApi.Tests.Services
+{
+    public class JediServiceTests
+    {
+        // não mexer
+        private readonly JediService _service;
+        private readonly Mock<IJediRepository> _repositoryMock;
+
+        public JediServiceTests()
+        {
+            // não mexer
+            _repositoryMock = new Mock<IJediRepository>();
+            _service = new JediService(_repositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task GetById_Success()
+        {
+            Assert.Equal(1, 4);
+        }
+
+        [Fact]
+        public async Task GetById_NotFound()
+        {
+            Assert.Equal(1, 4);
+        }
+
+        [Fact]
+        public async Task GetAll()
+        {
+            Assert.Equal(1, 4);
+        }
+    }
+}
